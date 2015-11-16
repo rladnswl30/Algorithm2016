02f4fd338ee98895fc4d465c67dac32c553327d2
Narae Kim <nrkim1122@gmail.com>
2015-11-17 02:23:55 +0900

    분산처리 -백준 : 제출시 시간 초과가 발생해 최적화 했지만 최적화 하니 오류 발생

--- 김나래/분산처리_백준[1009]_151116.cpp
+++ 김나래/분산처리_백준[1009]_151116.cpp
@@ -5,31 +5,19 @@ https://www.acmicpc.net/problem/1009
 */
 
 #include <stdio.h>
-#include <vector>
 
-using namespace std;
+long long modulo(int a, int b) {
+	long long cache[10] = { -1,-1,-1,-1,-1,-1,-1,-1,-1,-1 };
+	int i = -1, idx;
+	long long res = 1;
 
-int modulo(int a, int b){
-	int cache[11]={-1,};
-	int i=0;
-	unsigned long long res = 1;
-
-	while(1){
-		res = (res*a)%10;
-		if(cache[1] != res){
-			cache[i] = res;
-			//printf("cache[%d] = %llu\n",i,res);
-			++i;
-		} else 
-			break;
+	// cache[0]번은 쓰지 않음
+	while (cache[0] != (res = (res*a) % 10)) {
+		cache[++i] = res;
 	}
 
-	int idx;
-	printf("i == %d",i);
-	if((idx=b%(i-1) )==0)
-		idx = i-2;
-	printf("idx : %d\n",idx);
-	//printf("b mod size = %d: %d mod %d , cache[b mod size] : %d\n",b%size, b, size, cache[b%size]);
+	if ((idx = (b % (i + 1)) - 1) < 0)
+		idx = i;
 
 	return cache[idx];
 }
@@ -38,20 +26,13 @@ int main(){
 
 	int T=0;			// 테스트 케이스
 	int a=0,b=0;
-	vector<unsigned long long> res;
 	
 	scanf("%d",&T);
 	for(int i=0; i<T; ++i){
 		scanf("%d %d",&a,&b);
 
-		int k=modulo(a,b);
-		//puts("7");
-		res.push_back(k);
-		//puts("8");			
+		printf("%lld\n", modulo(a, b));
 	}
-
-	for(int i=0; i<res.size(); ++i)
-		printf("%llu\n",res[i]);
 
 	return 0;
 }
